eda9387e1957fc084ecce82132a32c2794e9ed43
SteveKimSR <kimsr96@naver.com>
2020-12-10 03:38:51 +0900

    implement setting function

--- server_directory/client_thread.py
+++ server_directory/client_thread.py
@@ -3,6 +3,7 @@ import threading
 
 import cv2
 import numpy as np
+import ast
 
 
 def work(conn: socket):
@@ -30,9 +31,20 @@ def work(conn: socket):
                 print("received")
 
             elif job_number == 2:  # 설정 변경 시그널
-                setting_size = int((conn.recv(5)).decode())
-                setting = conn.recv(setting_size).decode()
-                # 데이터베이스 세팅 변경
+                setting = int((conn.recv(4)).decode())
+                if setting == 0:
+                    text_size = int((conn.recv(4)).decode())
+                    text = conn.recv(text_size).decode()
+                    print(text)
+                    # 데이터베이스 매장명 변경 진행
+                    pass
+                elif setting == 1:
+                    point_size = int((conn.recv(4)).decode())
+                    point = ast.literal_eval(conn.recv(point_size).decode())
+                    color_size = int((conn.recv(4)).decode())
+                    color = ast.literal_eval(conn.recv(color_size).decode())
+                    # 데이터베이스에 포지션 넣기
+
     except ValueError:
         print('input data incorrect')
 
@@ -46,12 +58,12 @@ class socket_communicator:
         print(f"binded {tcp_ip}, {tcp_port}")
 
     def run(self):
-        t = threading.Thread(target=self.listener)
-        t.start()
+        self.listener()
 
     def listener(self):
-        self.s.listen(1)
-        conn, addr = self.s.accept()
-        print(f"connected to {addr[0]}")
-        t = threading.Thread(target=work, args=(conn,))
-        t.start()
+        while True:
+            self.s.listen(1)
+            conn, addr = self.s.accept()
+            print(f"connected to {addr[0]}")
+            t = threading.Thread(target=work, args=(conn,))
+            t.start()
