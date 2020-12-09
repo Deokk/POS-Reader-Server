e39bfed12cff2ce6bde762e5c8f90e565f1e3399
Deokk <deokk1112@gmail.com>
2020-12-10 02:45:19 +0900

    Database Test

--- node/routes/home.js
+++ node/routes/home.js
@@ -1,4 +1,8 @@
 const { func } = require("prop-types");
+const  db_config = require(__dirname + '../config/database.js');
+var conn = db_config.init();
+
+db_config.connect(conn);
 
 module.exports = function(app) {
     app.get('/', function(req, res) {
@@ -6,6 +10,10 @@ module.exports = function(app) {
     });
 
     app.get('/list', function(req, res) {
-        res.render('list.ejs');
+        var sql = 'SELECT * FROM store_info'
+        conn.query(sql, function(err, rows, fields) {
+            if(err) console.log('query is not excuted. select fail...\n' + err);
+            else res.render('list.ejs');
+        });
     });
 }
