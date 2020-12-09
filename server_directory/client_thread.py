41a26fba380b6d5215b15d9579bf2f5cf4d154ad
can019 <jys01012@gmail.com>
2020-12-10 06:21:25 +0900

    db 연결

--- server_directory/client_thread.py
+++ server_directory/client_thread.py
@@ -4,6 +4,7 @@ import threading
 import cv2
 import numpy as np
 import ast
+import dbConnector
 
 
 def work(conn: socket):
@@ -12,9 +13,14 @@ def work(conn: socket):
             company_id = int((conn.recv(4)).decode())
             job_number = int((conn.recv(4)).decode())
 
+            db = dbConnector.dbConnector('localhost', 3306, 'root', '0000', 'software_engineering');
+            print("dbConnection successed!")
+
             print(f"socket: c_id: {company_id}, j_num: {job_number} received")
             if job_number == 0:  # 매장 신규 생성 시그널
                 temp = 1000  # 데이터베이스에서 새로운 번호 할당받기
+                new_id = db.get_last_id()+1
+                print("새로운 아이디는 {}입니다.".format(new_id))
                 conn.sendall(str(temp).encode())
             if job_number == 1:  # 이미지 처리 시그널
                 img_size = int.from_bytes(conn.recv(4), byteorder="little")
@@ -23,15 +29,20 @@ def work(conn: socket):
                 img = cv2.imdecode(encoded_img, cv2.IMREAD_GRAYSCALE)
                 # cv2.imshow('gray', img)
                 # cv2.waitKey(0)
+
                 # 만약 데이터베이스에 id가 없다면?
-                # if company_id is DB 받을 부분:
-                # DB에 신규 ID 생성
-                #     return "생성된 company_ID는 {ID} 입니다. 매장 정보 설정을 통해 설정해주세요."
+                # if not db.is_id_exist(company_id):
+                #     # if company_id is DB 받을 부분:
+                #     # DB에 신규 ID 생성
+                #     #     return "생성된 company_ID는 {ID} 입니다. 매장 정보 설정을 통해 설정해주세요."
+                #     new_id = db.get_last_id()+1 # 신규 ID 배정
+                #     print("새로운 아이디는 {}입니다.".format(new_id))
+                #     #
+
                 # 재용이 함수 실행
                 # rate = check_Table.calc_rate(img, table_pointer)
                 # 데이터베이스 저장
                 print("received")
-
             elif job_number == 2:  # 설정 변경 시그널
                 setting = int((conn.recv(4)).decode())
                 if setting == 1:
@@ -39,6 +50,7 @@ def work(conn: socket):
                     point = conn.recv(point_size).decode()
                     color_size = int((conn.recv(4)).decode())
                     color = ast.literal_eval(conn.recv(color_size).decode())
+                    db.set_empty_color(company_id, text)
                     print(str(point))
                     # 데이터베이스에 포지션 넣기
                 else:
@@ -48,12 +60,15 @@ def work(conn: socket):
                     # 데이터베이스 매장명 변경 진행
                     if setting == 0:
                         # 매장명
+                        db.set_store_name(company_id, text)
                         pass
                     elif setting == 2:
                         # 매장주소
+                        db.set_store_location(company_id, text)
                         pass
                     elif setting == 3:
-                        # 수용인원
+                        # 수용테이블
+                        db.set_max_table_count_of_store(company_id, text)
                         pass
 
     except ValueError:
