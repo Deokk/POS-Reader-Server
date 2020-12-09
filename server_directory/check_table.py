787814b8d0586fb338754047568775aa5e7df447
jae-yong-2 <49151220+jae-yong-2@users.noreply.github.com>
2020-12-10 00:50:48 +0900

    bool to int

--- server_directory/check_table.py
+++ server_directory/check_table.py
@@ -5,9 +5,9 @@ def get_table(img, table_info):
     for num, i in enumerate(table_info):
         if not (img[int(i[0])][int(i[1])] == i[2]):
             if i[3]:
-                table_info[num] = [int(i[0]), int(i[1]), img[int(i[0])][int(i[1])], False]
+                table_info[num] = [int(i[0]), int(i[1]), img[int(i[0])][int(i[1])], 0]
             else:
-                table_info[num] = [int(i[0]), int(i[1]), img[int(i[0])][int(i[1])], True]
+                table_info[num] = [int(i[0]), int(i[1]), img[int(i[0])][int(i[1])], 1]
 
 
 def calc_rate(img, info):
