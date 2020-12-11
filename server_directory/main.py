765645fdd5107c44a05afd162cc508879e42e90c
SteveKimSR <kimsr96@naver.com>
2020-12-11 14:29:57 +0900

    final commit

--- server_directory/main.py
+++ server_directory/main.py
@@ -1,5 +1,5 @@
 from server_directory import client_thread
 
 if __name__ == '__main__':
-    s = client_thread.socket_communicator('localhost', 5001)
+    s = client_thread.socket_communicator('192.168.25.43', 5001)
     s.run()
